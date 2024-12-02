0f766608961829236285d5d2d5f3b8bfde92094c
Gergő Kirsch <kirsch.g.d@gmail.com>
2024-12-02 21:04:22 +0100

    home

--- Tetracosm appreciation/MainWindow.xaml.cs
+++ Tetracosm appreciation/MainWindow.xaml.cs
@@ -21,56 +21,118 @@ namespace Tetracosm_appreciation
     /// </summary>
     public partial class MainWindow : Window
     {
-        TranslateTransform trans = new TranslateTransform();
-        ScaleTransform scale = new ScaleTransform();
+        //transforms
         Random random = new Random();
-        int minute = 9;
-        int next_x = 0;
+        ScaleTransform scale = new ScaleTransform();
+        TranslateTransform trans = new TranslateTransform();
+
+        //movement
         int next_y = 0;
-        bool right = true;
+        int next_x = 0;
+        int movetimer = 45;
+
+        //modechange
+        string[] modes = { "fly", "hover", "sit", "rest", "sleep"};
+        string mode = "fly";
+        int changetimer = 0;
+
+        //animation
+        int framecount = 4;
+        int frame = 1;
+        string animname = "fly";
+
+        //startup
         public MainWindow()
         {
             InitializeComponent();
+            trans.X = -120;
+            trans.Y = random.Next(-100, 1200);
             Character.RenderTransform = trans;
             Character.LayoutTransform = scale;
             System.Windows.Threading.DispatcherTimer Main = new System.Windows.Threading.DispatcherTimer();
-            Main.Tick += Move;
-            Main.Interval = new TimeSpan(0, 0, 1);
+            Main.Tick += Tick;
+            Main.Interval = new TimeSpan(0, 0, 0, 0, 120);
             Main.Start();
+            Topmost = true;
         }
 
-        private void Screen_Deactivated(object sender, EventArgs e)
+        //keeping it on top
+        /*private void Screen_Deactivated(object sender, EventArgs e)
         {
             Topmost = true;
             Activate();
-        }
+        }*/
         
-        private void Move(object sender, EventArgs e)
+        //tick every 120 millisecs
+        private void Tick(object sender, EventArgs e)
         {
-            
-            display.Content = "X: " + Character.RenderTransform.Value.OffsetX + "\nY: " + Character.RenderTransform.Value.OffsetY;
-            minute++;
-            if (minute >= 10)
+            movetimer++;
+            changetimer++;
+            /*if (changetimer>random.Next(150,250))
+                mode = modes[random.Next(0,modes.Length)];*/
+
+            switch (mode)
             {
-                minute = 0;
+                case "fly":
+                    animname = "fly";
+                    fly();
+                    break;
+                case "sit":
+                    animname = "stand";
+                    sit();
+                    break;
+                case "rest":
+                    animname = "rest";
+                    rest();
+                    break;
+                case "sleep":
+                    animname = "sleep";
+                    sleep();
+                    break;
+                case "hover":
+                    animname = "fly";
+                    hover();
+                    break;
+                default:
+                    animname = "fly";
+                    fly();
+                    break;
+            }
+
+            Character.Source = new BitmapImage(new Uri(@"/Tetracosm appreciation;component/Images/" + animname + "-Frame-" + frame + ".png", UriKind.Relative));
+            frame++;
+            if (frame>framecount)
+            {
+                frame = 1;
+            }
+            
+        }
+
+        //flying around randomly
+        private void fly()
+        {
+            if (movetimer >= 50)
+            {
+                movetimer = 0;
                 int current_x = (int)Character.RenderTransform.Value.OffsetX;
                 int current_y = (int)Character.RenderTransform.Value.OffsetY;
-                next_x = random.Next((int)ActualHeight - 100);
-                next_y = random.Next((int)ActualWidth - 100);
-                DoubleAnimation anim1 = new DoubleAnimation{
+                next_y = random.Next((int)ActualHeight - 100);
+                next_x = random.Next((int)ActualWidth - 100);
+                DoubleAnimation anim1 = new DoubleAnimation
+                {
                     From = Character.RenderTransform.Value.OffsetX,
-                    To = next_y,
-                    Duration =  TimeSpan.FromSeconds(6),
+                    To = next_x,
+                    Duration = TimeSpan.FromSeconds(8),
                     EasingFunction = new ExponentialEase { EasingMode = EasingMode.EaseInOut, Exponent = 3 }
-                    };
+                };
                 DoubleAnimation anim2 = new DoubleAnimation
                 {
                     From = Character.RenderTransform.Value.OffsetY,
-                    To = next_x,
-                    Duration = TimeSpan.FromSeconds(6),
-                    EasingFunction = new ExponentialEase { EasingMode = EasingMode.EaseInOut, Exponent = 3 }
+                    To = next_y,
+                    Duration = TimeSpan.FromSeconds(8),
+                    EasingFunction = new ExponentialEase { EasingMode = EasingMode.EaseOut, Exponent = 1 }
                 }; ;
-                if (current_x<next_x)
+                if (current_x < next_x)
                 {
                     scale.ScaleX = 1;
                 }
@@ -81,6 +143,26 @@ namespace Tetracosm_appreciation
                 trans.BeginAnimation(TranslateTransform.XProperty, anim1);
                 trans.BeginAnimation(TranslateTransform.YProperty, anim2);
             }
+        }
+
+        private void sit()
+        {
+
+        }
+
+        private void rest()
+        {
+
+        }
+
+        private void sleep()
+        {
+
+        }
+
+        private void hover()
+        {
+
         }
     }
 }
